1a38cc827ecafd71775acea0ea803e4beee4ae81
Adrian Kolano <kolanoadrian@hotmail.com>
2021-03-17 21:51:43 +0100

    refactoring

--- script2.js
+++ script2.js
@@ -11,7 +11,7 @@ const Trade = (event) => {
     const form__inputFiat = document.querySelector(".form__inputFiat")
     const form__tradeResult = document.querySelector(".form__tradeResult")
 
-    
+
     const trade__CodeCountry = document.querySelector('[name="trade__CodeCountry"]').value;
     // const result__CodeCountry = document.querySelector('[name="result__CodeCountry"]').value;
     const url__trade = `http://api.nbp.pl/api/exchangerates/rates/A/${trade__CodeCountry}/`;
@@ -25,7 +25,7 @@ const Trade = (event) => {
             }
         })
         // .then((json) => console.log(json))
-        .then((json) => { 
+        .then((json) => {
             const tradeApi = json;
             console.log(tradeApi);
             console.log(tradeApi.rates[0].mid);
@@ -51,33 +51,31 @@ const Trade = (event) => {
                 let calculate__sell = kurs - spred;
                 const calculateResultSell = calculate__sell * input;
                 console.log(`domyślnie przeliczono na walutę Fiat PLN ${calculateResultSell}`);
-                
             }
         })
         .catch((error) => console.log(error, "błąd"))
-        const result__CodeCountry = document.querySelector('[name="result__CodeCountry"]').value;
-        const url__result = `http://api.nbp.pl/api/exchangerates/rates/A/${result__CodeCountry}/`;
-        fetch(url__result)
-            .then((answer) => {
-                console.log(answer)
-                if (answer.status !== 200) {
-                    throw Error("to nie jest odpowiedz 200")
-                } else {
-                    return answer.json()
-                }
-            })
-            // .then((json) => console.log(json))
-            .then((json) => { 
-                const resultApi = json;
-                console.log(resultApi);
-                console.log(resultApi.rates[0].mid);
-                const onChnange = resultApi.rates[0].mid;
-                console.log("Przelicz na: " + onChnange);
-              
-               
-            })
-            .catch((error) => console.log(error, "błąd"))
-        }
+        
+    const result__CodeCountry = document.querySelector('[name="result__CodeCountry"]').value;
+    const url__result = `http://api.nbp.pl/api/exchangerates/rates/A/${result__CodeCountry}/`;
+    fetch(url__result)
+        .then((answer) => {
+            console.log(answer)
+            if (answer.status !== 200) {
+                throw Error("to nie jest odpowiedz 200")
+            } else {
+                return answer.json()
+            }
+        })
+        // .then((json) => console.log(json))
+        .then((json) => {
+            const resultApi = json;
+            console.log(resultApi);
+            console.log(resultApi.rates[0].mid);
+            const onChnange = resultApi.rates[0].mid;
+            console.log("Przelicz na: " + onChnange);
+        })
+        .catch((error) => console.log(error, "błąd"))
+}
 
 const button = document.querySelector(".form__tradeButton")
-    button.addEventListener("click", Trade)
+button.addEventListener("click", Trade)
