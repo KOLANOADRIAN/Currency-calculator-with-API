dee42caaeb01914b4c735afe90079546f82cefbf
Adrian Kolano <kolanoadrian@hotmail.com>
2021-03-22 21:22:36 +0100

    add step in html ant script to get result on the page

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ const Trade = (event) => {
     const trade__buy = document.querySelector(".trade__buy");
     const trade__sell = document.querySelector(".trade__sell");
     const form__inputFiat = document.querySelector(".form__inputFiat")
-    const form__tradeResult = document.querySelector(".form__tradeResult")
-
- 
-
+    const form__resultExchange = document.querySelector(".form__resultExchange")
+    const form__resultBuy = document.querySelector(".form__resultBuy")
+    const formresultSell = document.querySelector(".formresultSell")
+    const form__resultTrade = document.querySelector(".form__resultTrade")
+    const form__resultInput = document.querySelector(".form__resultInput")
+    const form__resultOnChange = document.querySelector(".form__resultOnChange")
 
     const trade__CodeCountry = document.querySelector('[name="trade__CodeCountry"]').value;
     // const result__CodeCountry = document.querySelector('[name="result__CodeCountry"]').value;
@@ -32,30 +34,39 @@ const Trade = (event) => {
             console.log(tradeApi);
             console.log(tradeApi.rates[0].mid);
             const exchange = tradeApi.rates[0].mid;
+            console.log(exchange)
             console.log("Ilość do wymiany: " + form__inputFiat.value);
             const input = form__inputFiat.value;
-            // let calculate = kurs * input;
-            // console.log(calculate)
             const spred = 0.050;
             console.log("spred: " + spred);
+           
+            form__resultExchange.innerHTML = (`
+            <span class=""> Średni kurs NBP wybranej waluty: ${trade__CodeCountry} to: ${exchange} PLN<span><br>
+            <span class=""> spred: ${spred}<span><br>
+            `)
+
+            form__resultInput.innerHTML = (` <span class=""> Wprowadzona ilość ${trade__CodeCountry} do wymiany: ${input}<span><br>`)
             const buy = trade__buy.checked;
             console.log(trade__buy.checked);
-            if (buy === true) {
+            if (buy === true && sell === false) {
                 console.log("zaznaczono kup")
                 let calculate__buy = exchange + spred;
                 const calculateResultBuy = calculate__buy * input;
                 console.log(`domyślnie przeliczono na walutę Fiat PLN ${calculateResultBuy.toFixed(2)}`);
-                
+               
+                // .innerHTML = (`
+                // <span class="">Kurs kupna: ${calculate__buy}<span>
+                // `)
             }
             const sell = trade__sell.checked;
             console.log(trade__sell.checked);
-            if (sell === true) {
+            if (sell === true && buy === false) {
                 console.log("zaznaczono Sprzedaj")
                 let calculate__sell = kurs - spred;
                 const calculateResultSell = calculate__sell * input;
                 console.log(`domyślnie przeliczono na walutę Fiat PLN ${calculateResultSell.toFixed(2)}`);
             }
-            form__tradeResult.innerHTML = (`<span class=""> ${exchange}<span> `)
+            
             
            
            
